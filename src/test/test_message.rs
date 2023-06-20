9ff15982d04ee3229132935bb093a3788b0a68c6
not_joon <lbj199874@gmail.com>
2023-06-20 14:02:23 +0900

    update test

---
+++ src/test/test_message.rs
@@ -0,0 +1,9 @@
+#[cfg(test)]
+mod message_handling_test {
+    use std::sync::Arc;
+    use std::thread;
+    use std::time::Duration;
+
+    use crate::model::actor::ActorPool;
+    use crate::model::message::Message;
+}
