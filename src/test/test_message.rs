bde9e01824b9addeead6cea7ec81d871806740a4
not_joon <lbj199874@gmail.com>
2023-06-26 16:19:18 +0900

    add test

--- src/test/test_message.rs
+++ src/test/test_message.rs
@@ -27,42 +27,69 @@ mod message_handling_test {
     }
 
     #[test]
-    fn test_send_message_to_actors() {
-        let pool = ActorPool::new();
+    fn test_multiple_messages() {
+        let actor = ActorPool::new();
+        let id = actor.create_actor();
 
-        let actor_ids: Vec<usize> = (0..10).map(|_| pool.create_actor()).collect();
+        let value = actor.get_actor_value(id).unwrap();
+        assert_eq!(value, 0);
 
-        for actor_id in actor_ids.iter() {
-            if actor_id % 2 == 0 {
-                pool.message_loop(*actor_id, Message::Increment(10))
-                    .unwrap();
-            }
+        println!("id: {}, value: {}", id, value);
+
+        let increment_values = vec![10, 20, 55, 130];
+        for value in increment_values {
+            let message = Message::Increment(value);
+            actor.message_loop(id, message).unwrap();
         }
 
-        for actor_id in actor_ids.iter() {
-            let value = pool.get_actor_value(*actor_id).unwrap();
+        thread::sleep(std::time::Duration::from_millis(100));
 
-            println!("id: {}, value: {}", actor_id, value);
+        let value = actor.get_actor_value(id).unwrap();
+        assert_eq!(value, 215);
 
-            if actor_id % 2 == 0 {
-                assert_eq!(value, 10);
-            }
-            assert_eq!(value, 0);
+        let decrement_values = vec![10, 20, 55, 130];
+        for value in decrement_values {
+            let message = Message::Decrement(value);
+            actor.message_loop(id, message).unwrap();
         }
+
+        thread::sleep(std::time::Duration::from_millis(100));
+
+        let value = actor.get_actor_value(id).unwrap();
+        assert_eq!(value, 0);
     }
 
     #[test]
     fn test_message_propagation() {
         let pool = ActorPool::new();
 
-        let actor_ids: Vec<usize> = (0..10).map(|_| pool.create_actor()).collect();
-        let target_actor_id = actor_ids[0];
+        let actor1 = pool.create_actor();
+        let actor2 = pool.create_actor();
+        let actor3 = pool.create_actor();
 
-        let subscriber_actor_ids = actor_ids[1..5].to_vec();
+        let actor1_value = pool.get_actor_value(actor1).unwrap();
+        let actor2_value = pool.get_actor_value(actor2).unwrap();
+        let actor3_value = pool.get_actor_value(actor3).unwrap();
 
-        pool.subscribe(target_actor_id, subscriber_actor_ids)
-            .unwrap();
+        assert_eq!(actor1_value, 0);
+        assert_eq!(actor2_value, 0);
+        assert_eq!(actor3_value, 0);
 
-        // TODO: Message propagation test
+        // Add actor2 and actor3 to actor1's subscription list
+        pool.subscribe(actor1, vec![actor2, actor3]).unwrap();
+
+        // Send message to actor1
+        let message = Message::Increment(10);
+        pool.message_loop(actor1, message).unwrap();
+
+        thread::sleep(std::time::Duration::from_millis(100));
+
+        let new_actor1_value = pool.get_actor_value(actor1).unwrap();
+        let new_actor2_value = pool.get_actor_value(actor2).unwrap();
+        let new_actor3_value = pool.get_actor_value(actor3).unwrap();
+
+        assert_eq!(new_actor1_value, 10);
+        assert_eq!(new_actor2_value, 10);
+        assert_eq!(new_actor3_value, 10);
     }
 }
